eca468d22abeb33bef57d412b95d8fe6e2a037c4
Daanilo <danilo.dna.rna@gmail.com>
2015-05-25 13:29:02 -0300

    Fim do modulo 1
    terminei o projeto com um relógio que atualiza a hora local a cada 250
    mili-segundo

--- MVATML5/MVATML5/js/default.js
+++ MVATML5/MVATML5/js/default.js
@@ -15,6 +15,12 @@
         ClasseTexto: WinJS.Class.define(function (element, options) {
             element.style.fontSize = "72px";
             element.wincontrol = this;
+        }),
+
+        //elemento visual
+        model: WinJS.Binding.as({
+            hora: new Date().toLocaleTimeString()
+
         })
 
     });
@@ -25,6 +31,16 @@
         //instanciar a classe texto
         //new MVAHTML5.ClasseTexto(DivPrincipal)
 
+        // vinculo do elemento visual com a div principal
+        MVAHTML5.model.bind("hora", function (value) {
+            DivPrincipal.textContent = value;
+
+        });
+
+        //intervalo para atualizar a div principal 1/4 de segundo
+        setInterval(function () {
+            MVAHTML5.model.hora = new Date().toLocaleTimeString();
+        }, 250);
 
         WinJS.UI.processAll();
     };

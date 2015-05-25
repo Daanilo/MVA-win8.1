20a2e174a90d8ab6a5fdbc466092f2cb4dc85cf1
Daanilo <danilo.dna.rna@gmail.com>
2015-05-25 13:04:22 -0300

    Upload Primeira Video aula MVA para windows 8.1
    subi tudo

---
+++ MVATML5/MVATML5/js/default.js
@@ -0,0 +1,41 @@
+﻿(function () {
+    //modo estrito evita erros variados como não declarar uma variável
+    "use  strict";
+
+    //apelidamos o objeto WinJS só pra facilitar a digitação
+    var app = WinJS.Application;
+
+    //Definir o Namespace  
+    WinJS.Namespace.define("MVAHTML5", {
+        id: function (id) {
+            return document.getElementById(id);
+        },
+
+        // Definir uma classe para editar o texto
+        ClasseTexto: WinJS.Class.define(function (element, options) {
+            element.style.fontSize = "72px";
+            element.wincontrol = this;
+        })
+
+    });
+
+    app.onactivated = function (e) {
+        var DivPrincipal = MVAHTML5.id("DivPrincipal");
+        DivPrincipal.textContent = "Olá mundo MVA HTML5 Windows 8.1!";
+        //instanciar a classe texto
+        //new MVAHTML5.ClasseTexto(DivPrincipal)
+
+
+        WinJS.UI.processAll();
+    };
+
+    app.start();
+
+
+
+
+
+
+
+// encapsulamento e cóigo java script ()
+})();
